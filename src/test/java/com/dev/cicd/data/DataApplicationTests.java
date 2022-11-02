e0d3376cfb2e49775f907aaafcea4b0fe1106851
Anthony <anthony.pizzulli1@gmail.com>
2022-11-02 11:53:06 -0400

    fized test issue

--- src/test/java/com/dev/cicd/data/DataApplicationTests.java
+++ src/test/java/com/dev/cicd/data/DataApplicationTests.java
@@ -1,6 +1,9 @@
 package com.dev.cicd.data;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
@@ -9,5 +12,34 @@ class DataApplicationTests {
 	@Test
 	void contextLoads() {
 	}
+	@SpringBootTest
+class DataControllerTest {
+
+    @Autowired
+    DataController dataController;
+
+    @Test
+    void health() {
+        assertEquals( "HEALTH CHECK OK!", dataController.healthCheck());
+    }
+
+    @Test
+    void version() {
+        assertEquals( "The actual version is 1.0.0", dataController.version());
+    }
+
+    @Test
+    void nationsLength() {
+        Integer nationsLength = dataController.getNations().size();
+        assertEquals(10, nationsLength);
+    }
+
+    @Test
+    void currenciesLength() {
+        Integer currenciesLength = dataController.getRandomCurrencies().size();
+        assertEquals(20, currenciesLength);
+    }
+
+}
 
 }
